1fdca4e5ec19ca5350961a2847498b5e35f2ac88
Armaan Verma <armaanverma30330@gmail.com>
2025-09-18 15:09:49 +0530

    synchronus

---
+++ JS/jssync.js
@@ -0,0 +1,28 @@
+function register() {
+    console.log("register end");
+}
+
+function sendEmail() {
+    console.log("send email end");
+}
+
+function login() {
+    console.log("login end");
+}
+
+function getData() {
+    console.log("get data end");
+}
+
+function displayData() {
+    console.log("display data end");
+}
+
+
+register();
+sendEmail();
+login();
+getData();
+displayData();
+
+console.log("other application work");
