44b1ff2437b1511a993facba5c1a423e73acc5f4
Armaan Verma <armaanverma30330@gmail.com>
2025-10-30 15:17:45 +0530

    VITE

---
+++ Book-VITE-APP/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Book from './component/Book'
+import Header from './component/Header'
+import Footer from './component/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    </div classname='booklist'>
+    {
+      data.map(b,i)
+    }
+    </div>
+    <Footer/>
+   
+
+    </div>
+  )
+}
+
+export default App
