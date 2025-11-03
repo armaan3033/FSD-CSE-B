2836aa22f80ce95173ab461c23ad7e6f2033d260
Armaan Verma <armaanverma30330@gmail.com>
2025-11-03 15:30:19 +0530

    Shopping cart

---
+++ Shopping-Vite-App/src/component/Book.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Book.css'
+import './App.css'
+const Book = () => {
+  return (
+    <div className='card'>
+      <img src="" width={200} height={200} alt='image'/>
+      <h3> Title</h3>
+    </div>
+  )
+}
+
+export default Book
