4269d31922e8bf3db959617b10db171929924e9f
Armaan Verma <armaanverma30330@gmail.com>
2026-02-23 15:34:36 +0530

    newcrud

--- crudserver/crudserver.js
+++ crudserver/crudserver.js
@@ -1,7 +1,7 @@
 import http from 'http'
 const port=5000;
-const users=[{id:1,name:'ABC',email:"abc@getMaxListeners.com"},
-            {id:1,name:'ABC',email:"abc@getMaxListeners.com"}
+const users=[{id:1,name:'ABC',email:"abc@gmail.com"},
+            {id:2,name:'XYZ',email:"xyz@gmail.com"}
 ]
 const server=http.createServer((req, res)=>{
     const url= req.url;
@@ -13,11 +13,33 @@ const server=http.createServer((req, res)=>{
         res.end(JSON.stringify(users));
     }
     else if(url.startsWith("/users/")&& method =="GET"){
-        res.end(JSON.stringify(users));
+        const id=url.split("/")[2];
+        const user=users.find(u=>u.id==id);
+        if(!user){
+            res.statusCode=400;
+            res.end("User not found");
+        }
     }
     else if(url=="/createusers"&& method =="POST"){
-        res.statusCode=201;
-        res.end("Create user");
+        let body="";
+
+        res.on("data",(chunk)=>{
+            body=body+chunk;
+        })
+        req.on("end",()=>{
+            const data=JSON.parse(body);
+            const newUser={
+                id:Date.now(),
+                name:data.name,
+            }
+            if (data.name==null && data.email==null){
+                res.statusCode=400;
+                res.end(`User id ${newUser.id}  `);
+                
+            }
+            
+        })
+        
     }
     else if(url.startsWith("/users/")&& method =="GET"){
         res.end("Edit user");
